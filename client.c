d8f9597c952a882b6dcc1760128d9387e194b075
Tanner Smith <tanner@tssoftware.net>
2012-11-10 16:45:51 -0500

    Add CLI arguments parsing in the client.

--- client.c
+++ client.c
@@ -8,27 +8,30 @@
 
 #include "dropper.h"
 
-#define DEFAULT_SERVER_PORT 7
-
 int debug;
 
 int main(int argc, char *argv[]) {
-	char *server_ip;
-	unsigned int server_port;
+	char *server_ip, *remote_filename, *local_filename;
+	unsigned int server_port, max_packet_size;
+    unsigned short loss_percentage;
 
-	if ((argc < 3) || (argc > 3)) {
-	   fprintf(stderr, "Usage: %s <Server IP> [<Echo Port>]\n", argv[0]);
+    if (argc != 7) {
+	   fprintf(stderr, "Usage: %s <Server IP> <Server Port> <Remote File> <Local File> <Max Packet Size> <Loss Percentage>\n", argv[0]);
 	   exit(1);
 	}
 
-	server_ip = argv[1]; // First argument – Server IP (dotted quad)
+    server_ip = argv[1];
+    server_port = atoi(argv[2]);
+    remote_filename = argv[3];
+    local_filename = argv[4];
+    max_packet_size = atoi(argv[5]);
+    loss_percentage = atoi(argv[6]);
 
-	// Use a given port if one is provided, otherwise the default
-	if (argc == 3) {
-		server_port = atoi(argv[2]);
-	} else {
-		server_port = DEFAULT_SERVER_PORT;
-	}
+    // Set the dropper loss percentage
+    if (set_dropper(loss_percentage) < 0) {
+        fprintf(stderr, "Invalid loss percentage - Must be between 0 and 100.");
+        exit(2);
+    }
 
 	// Create a socket using UDP
 	int sock;
