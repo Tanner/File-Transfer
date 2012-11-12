d0c989ac700977221cee144c1a01010ed21047f3
Tanner Smith <tanner@tssoftware.net>
2012-11-12 15:22:08 -0500

    Implement simple ACK on recv and send waits for ACK.
    Does not care about who's ACK it is, just wants one.

--- arq.c
+++ arq.c
@@ -18,9 +18,45 @@ int arq_init(int loss_percentage) {
 }
 
 ssize_t arq_sendto(int sock, void *buffer, size_t len, int flags, struct sockaddr *dest_addr, int addr_len) {
-    return sendto_dropper(sock, buffer, len, flags, dest_addr, addr_len);
+    int size = sendto_dropper(sock, buffer, len, flags, dest_addr, addr_len);
+
+    char recv_buffer[BUFFER_MAX_SIZE];
+    int recv_buffer_size;
+
+    if ((recv_buffer_size = recvfrom(sock, recv_buffer, BUFFER_MAX_SIZE, 0, 0, 0)) < 0) {
+        printf("Recvd Buffer Size %d\n", recv_buffer_size);
+        fprintf(stderr, "Could not receive message from server.");
+        exit(2);
+    }
+
+    printf("Received a message from the server!\n");
+
+    int split_size = 0;
+    char **split_buffer = split(recv_buffer, " ", &split_size);
+
+    if (strcmp(split_buffer[0], "ACK") == 0) {
+        printf("Got an ACK!\n");
+    }
+
+    return size;
 }
 
-ssize_t arq_recvfrom(int sock, void *buffer, size_t len, int flags, struct sockaddr *src_addr, int addr_len) {
-    return recvfrom(sock, buffer, len, flags, src_addr, (socklen_t *) addr_len);
+ssize_t arq_recvfrom(int sock, void *buffer, size_t len, int flags, struct sockaddr *src_addr, int *addr_len) {
+    int size = recvfrom(sock, buffer, len, flags, src_addr, (socklen_t *) addr_len);
+
+    arq_ack(sock, src_addr, *addr_len);
+    
+    return size;
+}
+
+ssize_t arq_ack(int sock, struct sockaddr *dest_addr, int addr_len) {
+    char *buffer = "ACK";
+
+    int size = sendto_dropper(sock, buffer, strlen(buffer), 0, dest_addr, addr_len);
+
+    if (debug || 1) {
+        printf("Sent an ACK\n");
+    }
+
+    return size;
 }
--- arq.h
+++ arq.h
@@ -14,6 +14,8 @@ int debug;
 int arq_init(int loss_percentage);
 
 ssize_t arq_sendto(int sock, void *buffer, size_t len, int flags, struct sockaddr *dest_addr, int addr_len);
-ssize_t arq_recvfrom(int sock, void *buffer, size_t len, int flags, struct sockaddr *src_addr, int addr_len);
+ssize_t arq_recvfrom(int sock, void *buffer, size_t len, int flags, struct sockaddr *src_addr, int *addr_len);
+
+ssize_t arq_ack(int sock, struct sockaddr *dest_addr, int addr_len);
 
 #endif
